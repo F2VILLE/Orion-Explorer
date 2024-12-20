2136ca6bae4f2d2b606860aa77974b4eb5b5b406
= <feliciendeville@gmail.com>
2024-12-21 00:04:58 +0100

    Display folders from path

--- tauri-app/src-tauri/src/lib.rs
+++ tauri-app/src-tauri/src/lib.rs
@@ -1,14 +1,53 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn get_files() -> Result<Vec<String>, String> {
-    let files = std::fs::read_dir("/").map_err(|e| e.to_string())?;
-    let files = files
-        .filter_map(|file| file.ok())
-        .filter_map(|file| file.path().to_str().map(|s| s.to_string()))
-        .collect();
-    Ok(files)
+use std::time::SystemTime;
+
+#[derive(serde::Serialize)]
+struct FileObject {
+    name: String,
+    size: u64,
+    is_dir: bool,
+    created: u64,
+    modified: u64,
+    thumbnail: String,
+    path: String,
 }
 
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn get_files(path: String) -> Result<Vec<FileObject>, String> {
+    let files = std::fs::read_dir(path).map_err(|e| e.to_string())?;
+    let files: Vec<String> = files
+        .filter_map(|file: Result<std::fs::DirEntry, std::io::Error>| file.ok())
+        .filter_map(|file| file.path().to_str().map(|s| s.to_string()))
+        .collect();
+    let res: Vec<FileObject> = files.iter().filter(|file| {
+        let metadata = std::fs::metadata(file).unwrap();
+        metadata.is_dir() || metadata.is_file()
+    }).collect::<Vec<_>>()
+    .iter()
+        .map(|file| {
+            let metadata = std::fs::metadata(file).unwrap();
+            let created = metadata.created().unwrap();
+            let modified = metadata.modified().unwrap();
+            let thumbnail = if metadata.is_dir() {
+                "folder".to_string()
+            } else {
+                "file".to_string()
+            };
+            FileObject {
+                name: file.clone().split('/').last().unwrap().to_string(),
+                size: metadata.len(),
+                is_dir: metadata.is_dir(),
+                created: created.elapsed().unwrap().as_millis() as u64,
+                modified: modified.elapsed().unwrap().as_millis() as u64,
+                thumbnail,
+                path: file.clone().to_string(),
+            }
+        })
+        .collect();
+    
+
+    Ok(res)
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
--- tauri-app/src/App.tsx
+++ tauri-app/src/App.tsx
@@ -3,41 +3,68 @@ import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
-type FileObject = {
-  name: string;
-  size: number;
-  created: string;
-  modified: string;
-  thumbnail: string;
-  path: string;
+/*
+type FileObject as the following :
+struct FileObject {
+    name: String,
+    size: u64,
+    is_dir: bool,
+    created: SystemTime,
+    modified: SystemTime,
+    thumbnail: String,
+    path: String,
+}
+
+*/
+
+type FileObject =  {
+  name: string,
+  size: number,
+  is_dir: boolean,
+  created: string,
+  modified: string,
+  thumbnail: string,
+  path: string,
 }
 
 function App() {
-  const [files, setFiles] = useState<string[]>([]);
-
+  const [files, setFiles] = useState<FileObject[]>([]);
+  const [dirPath, setDirPath] = useState("/home/f2ville");
   const getFiles = async () => {
-    const response = await invoke("get_files");
-    setFiles(response as string[]);
+    const response = await invoke("get_files", {
+      path: dirPath
+    });
+    const lesFichiers = (response as FileObject[]).sort((a, b) => {
+      if (a.is_dir === b.is_dir) {
+        return a.name.localeCompare(b.name);
+      } else {
+        return a.is_dir ? -1 : 1;
+      }  
+  })
+  console.log(lesFichiers);
+    setFiles(lesFichiers);
   };
 
   useEffect(() => {
     getFiles();
-  }, []);
+  }, [dirPath]);
 
   return (
     <main className="container">
       <h1>Orion File Explorer</h1>
       <div>
         {files.length > 0 ? (
-          <ul>
+          <ul className="files">
             {files.map((file, index) => (
-              <li key={index}>
-                {/* <img src={file.thumbnail} alt={file.name} />
+              <li key={index} className="file">
+                <img src={file.thumbnail} alt={file.thumbnail} />
                 <p>{file.name}</p>
+                <div>
                 <p>{file.size} bytes</p>
                 <p>Created: {file.created}</p>
-                <p>Modified: {file.modified}</p> */}
-                <p>{file}</p>
+                <p>Modified: {file.modified}</p>
+
+                </div>
               </li>
 
             ))}
